22ae7fb7c7d60ecef1f60c100a9b08a61de5e95d
Vignesh G <158274623+VigneshG26@users.noreply.github.com>
2024-08-20 18:43:23 +0530

    js file added

---
+++ location.js
@@ -0,0 +1,17 @@
+navigator.geolocation.getCurrentPosition(function(position){
+    const {latitude, longitude} = position.coords
+    coord = [latitude, longitude]
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+    console.log('Accuracy of location:', position.coords.accuracy);
+    var map = L.map('map').setView(coord, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coord).addTo(map)
+        .bindPopup('Your Location')
+        .openPopup();
+},function(){
+    alert('location not found')
+})
